87e9e1be4996b832b40a92242767a6b59503835a
bbreck3 <bbreck3@gmail.com>
2016-04-30 20:05:07 -0400

    Manhattan Distance implemented

---
+++ A4/ManhattanSample.py
@@ -0,0 +1,78 @@
+#all subclasses inherit sample and calculate the data from the list created in sample in a specific way
+#inherits sample
+#Euclidean measure the EuclideanDistance of all the elements
+#EuclideanNorm is the the sqrt(summation(i^2))) or the square root of all summatino each ith term squared 
+import math;
+from sample import sample;
+class ManhattanSample(sample):
+	def _init_(self,list):
+		super(ManhattanSample,self)._init_(list);
+		
+		#Eucliudean Distance
+	def distance(self,list1,list2):
+		list3=range(4);
+		for num in range(0,len(list2)):
+ 			
+ 			list3[num] = list1[num]-list2[num];
+ 		ED = abs(list3[0]+list3[1]+list3[2]+list3[3]);
+ 		
+		return ED;
+		#super(EuclideanSample,self).distance(list1,list2);
+
+		
+	def getList(self):
+ 		return self.list;
+
+ 	#Inherited getList Method
+	#def getList(self):
+		#return super(EuclideanSample, self).getList();
+
+
+	#def _init_(self,list):
+		#self.list = list;
+	#def getList(self):
+		#return self.list;
+	#def setList(self, list):
+		#this.list = list;
+	
+
+##################################
+###    Initialize the Lists    ###
+##################################
+
+#initialize a sample list and a list from the Euclidean list
+list = [1,2,3,4];
+a= ManhattanSample();
+a._init_([5,6,7,8]);
+
+
+
+############################
+###    Step by Step     ####
+############################
+
+#get the Euclidean sampe list
+#z = a.getList();
+#print(z);
+#print(list);
+
+#calculate the distance between the Euclidean sample and the sample list
+#x = a.distance(z, list);
+#print out the distance
+#print(x);
+
+
+##########################
+###    All in One     ####
+##########################
+
+#print the results in one line
+print(a.distance(a.getList(), list));
+
+
+#print(z);
+#print(x);
+#print(b.distance(a.getList(),b.getList()));
+
+
+
